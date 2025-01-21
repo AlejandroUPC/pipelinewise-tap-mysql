a9660257baf364f0c901c6f8c2be51ec7c272a15
Alejandro Martinez Otal <alejandro.martinezotal@mylighthouse.com>
2025-01-21 16:02:08 +0100

    update pin to 0451

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(name='pipelinewise-tap-mysql',
       install_requires=[
           'pendulum==2.1.2',
           'pipelinewise-singer-python==1.*',
-          'mysql-replication @ git+https://github.com/AlejandroUPC/python-mysql-replication.git#egg=python-mysql-replication.git#egg=python-mysql-replication',
+          'mysql-replication==0.45.1',
           'PyMySQL==1.1.*',
           'plpygis==0.2.1',
           'tzlocal==2.1',
